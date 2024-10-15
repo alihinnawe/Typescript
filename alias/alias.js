12005a243d9e7a1683567e308ea7d55292160d89
hinnaweali <hinnaweali@gmail.com>
2024-10-15 12:37:22 +0200

    add more exercise commit

---
+++ alias/alias.js
@@ -0,0 +1,12 @@
+var player1 = {
+    name1: "ali",
+    color: "red",
+    position: 1
+};
+var player2 = {
+    name1: "ali1",
+    color: "red1",
+    position: 11
+};
+alert(player1.color);
+alert(player2.name1);
---
+++ alias/alias.ts
@@ -0,0 +1,26 @@
+
+type Player = {
+
+    name1 : string; 
+    color :string;
+    position: number;
+
+}
+
+
+ let player1 : Player = { 
+    name1 : "ali",
+    color : "red",
+    position : 1
+ }
+
+
+ 
+ let player2 : Player = { 
+    name1 : "ali1",
+    color : "red1",
+    position : 11
+ }
+
+ alert(player1.color);
+ alert(player2.name1);
